7ef30041853ee1ffd81c80a98ee1f2fd96ede896
Utkarsh Pathak <axdfgh111@gmail.com>
2022-11-29 12:09:02 +0530

    Made o/p format changes

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import numpy as np
 import copy
 from core.filter import GuidedFilter
 
-INPUT_IMAGE_PATH = ".\input images\dark4.jpg"
-OUTPUT_IMAGE_PATH = ".\output images\dark4.jpg"
+#All the tweakable parameters
+INPUT_IMAGE_PATH = ".\input images\dark3.jpg"
+OUTPUT_IMAGE_PATH = ".\output images"
 gamma = 2
 alpha = 0.5
 radius = 20
 eps = 0.001
+
+
 def custom_hist(img_array):
     """
     STEP 1: Normalized cumulative histogram
@@ -41,17 +44,8 @@ img = cv2.imread(INPUT_IMAGE_PATH)
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)[:,:,2]
 # cv2.imshow("Gray image", gray_img)
 
-for y in range(gray_img.shape[0]):
-    for x in range(gray_img.shape[1]):
-        intensity = 0
-        for color in img[y,x]:
-            intensity += color
-        intensity /= 3
-        gray_img[y,x] = intensity
-cv2.namedWindow("Input Image", cv2.WINDOW_NORMAL)
-cv2.imshow("Input Image", img)
-cv2.namedWindow("Grayed Input Image", cv2.WINDOW_NORMAL)
-cv2.imshow("Grayed Input Image", gray_img)
+cv2.imwrite(OUTPUT_IMAGE_PATH + "\\input_image.jpg", img)
+cv2.imwrite(OUTPUT_IMAGE_PATH + "\\grayed_input_image.jpg", gray_img)
 
 new_img = copy.deepcopy(gray_img)
 
@@ -59,8 +53,8 @@ new_img = copy.deepcopy(gray_img)
 for y in range(gray_img.shape[0]):
     for x in range(gray_img.shape[1]):
             new_img[y,x] = int(255 * ((gray_img[y,x]/255)**(1/gamma)))
-cv2.namedWindow("Gamma corrected", cv2.WINDOW_NORMAL)
-cv2.imshow("Gamma corrected",new_img)
+
+cv2.imwrite(OUTPUT_IMAGE_PATH + "\\gamma_corrected.jpg", new_img)
 
 hist_img = cv2.equalizeHist(gray_img)   
 # hist_img = custom_hist(gray_img)
@@ -68,27 +62,27 @@ for y in range(gray_img.shape[0]):
     for x in range(gray_img.shape[1]):
             new_img[y,x] = (1 - alpha)*new_img[y,x] + alpha*hist_img[y,x]
 
-cv2.namedWindow("Histogram Equalized", cv2.WINDOW_NORMAL)
-cv2.imshow("Histogram Equalized", hist_img)
-cv2.namedWindow("Alpha Blended", cv2.WINDOW_NORMAL)
-cv2.imshow("Alpha Blended", new_img)
+cv2.imwrite(OUTPUT_IMAGE_PATH + "\\hist_equalized_image.jpg", hist_img)
+
+cv2.imwrite(OUTPUT_IMAGE_PATH + "\\alpha_blended_image.jpg", new_img)
+
 
 Oimg = copy.deepcopy(new_img)
 ret, thresh1 = cv2.threshold(new_img, 0, 255, cv2.THRESH_BINARY +  cv2.THRESH_OTSU)
-cv2.namedWindow("Otsu bin", cv2.WINDOW_NORMAL)
-cv2.imshow("Otsu bin", thresh1)
+cv2.imwrite(OUTPUT_IMAGE_PATH + "\\otsu's_threshold.jpg", thresh1)
+
 GF = GuidedFilter(new_img, radius, eps)
 
 gf_res = GF.filter(thresh1)
-cv2.namedWindow("Guided Filter Mask", cv2.WINDOW_NORMAL)
-cv2.imshow("Guided Filter Mask", gf_res) 
+cv2.imwrite(OUTPUT_IMAGE_PATH + "\\guided_filter_mask.jpg", gf_res)
+
 
 for x in range(Oimg.shape[0]):
     for y in range(Oimg.shape[1]):
         Oimg[x,y] = gf_res[x,y]*Oimg[x,y] + (1-gf_res[x,y])*gray_img[x,y]
 
-cv2.namedWindow("Grayed output", cv2.WINDOW_NORMAL)
-cv2.imshow("Grayed output", Oimg)
+cv2.imwrite(OUTPUT_IMAGE_PATH + "\\grayed_output.jpg", new_img)
+
 O_rgb = copy.deepcopy(cv2.cvtColor(img, cv2.COLOR_BGR2HSV))
 O_rgb[:,:,2] = Oimg
 O_rgb = cv2.cvtColor(O_rgb, cv2.COLOR_HSV2BGR)
@@ -98,9 +92,9 @@ O_rgb = cv2.cvtColor(O_rgb, cv2.COLOR_HSV2BGR)
 #     for x in range(img.shape[1]):
 #         for c in range(img.shape[2]):
 #             O_rgb[y,x,c] = img[y,x,c]*(Oimg[y,x]/gray_img[y,x])
-cv2.namedWindow("Colored Output", cv2.WINDOW_NORMAL)
-cv2.imshow("Colored Output", O_rgb)
-cv2.imwrite(OUTPUT_IMAGE_PATH, np.hstack((img, O_rgb)))
+cv2.imwrite(OUTPUT_IMAGE_PATH + "\\colored_output.jpg", new_img)
+
+cv2.imwrite(OUTPUT_IMAGE_PATH + "\\final_comparison.jpg", np.hstack((img, O_rgb)))
 
 cv2.waitKey(0) 
 cv2.destroyAllWindows() 
